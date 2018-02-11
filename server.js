a2c802e0cbed3cc5e7050a874678bff63089c86f
Alexis <alexis.dardenne7@gmail.com>
2018-02-11 20:22:07 +0100

    Add socket

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
-var express = require('express');
-
-var app = express();
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(req, res) {
 	res.render('chat.ejs');
 });
 
+io.on('connection', function(socket){
+	console.log('A user connected');
+	socket.on('disconnect', function(){
+		console.log('User disconnected');
+	});
+});
+
 const port = process.env.PORT || 8080;
-app.listen(port);
+http.listen(port);
